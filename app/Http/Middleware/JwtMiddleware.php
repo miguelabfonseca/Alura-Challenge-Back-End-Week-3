c24ebe20ca6bbe8d13d36457f5005d92c73808f1
Miguel António <miguelabfonseca@gmail.com>
2022-11-21 15:04:57 +0000

    [UPD] jwt

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -19,25 +19,19 @@ class JwtMiddleware extends BaseMiddleware
      */
     public function handle($request, Closure $next)
     {
-        echo "0";
         try {
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof TokenInvalidException) {
-                var_dump("1");
                 return response()->json(['status' => 'Token is Invalid'], 403);
             } else if ($e instanceof TokenInvalidException) {
-                var_dump("2");
                 return response()->json(['status' => 'Token is Expired'], 401);
             } else if ($e instanceof TokenInvalidException) {
-                var_dump("3");
                 return response()->json(['status' => 'Token is Blacklisted'], 400);
             } else {
-                var_dump("4");
                 return response()->json(['status' => 'Authorization Token not found'], 404);
             }
         }
-        echo "5";
 
         return $next($request);
     }
